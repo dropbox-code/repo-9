seaunderwater/cluster-api-provider-aws

support overriding security groups in AWSCluster spec

api/v1alpha3/awscluster_types.go
@@ -131,6 +131,11 @@ type AWSLoadBalancerSpec struct {
 	// Subnets sets the subnets that should be applied to the control plane load balancer (defaults to discovered subnets for managed VPCs or an empty set for unmanaged VPCs)
 	// +optional
 	Subnets []string `json:"subnets,omitempty"`
+
+	// SecurityGroups sets the security groups used by the load balancer. Expected to be security group IDs.
+	// This is optional - if not provided new security groups will be created for the load balancer
+	// +optional
+	SecurityGroups []string `json:"securityGroups,omitempty"`
 }
 
 // AWSClusterStatus defines the observed state of AWSCluster

pkg/cloud/services/elb/loadbalancer.go
@@ -301,6 +301,15 @@ func (s *Service) getAPIServerClassicELBSpec() (*infrav1.ClassicELB, error) {
 	if err != nil {
 		return nil, err
 	}
+
+	securityGroupIDs := []string{}
+	controlPlaneLoadBalancer := s.scope.ControlPlaneLoadBalancer()
+	if controlPlaneLoadBalancer != nil && len(controlPlaneLoadBalancer.SecurityGroups) != 0 {
+		securityGroupIDs = append(securityGroupIDs, controlPlaneLoadBalancer.SecurityGroups...)
+	} else {
+		securityGroupIDs = append(securityGroupIDs, s.scope.SecurityGroups()[infrav1.SecurityGroupControlPlane].ID)
+	}
+
 	res := &infrav1.ClassicELB{
 		Name:   elbName,
 		Scheme: s.scope.ControlPlaneLoadBalancerScheme(),
@@ -319,7 +328,7 @@ func (s *Service) getAPIServerClassicELBSpec() (*infrav1.ClassicELB, error) {
 			HealthyThreshold:   5,
 			UnhealthyThreshold: 3,
 		},
-		SecurityGroupIDs: []string{s.scope.SecurityGroups()[infrav1.SecurityGroupAPIServerLB].ID},
+		SecurityGroupIDs: securityGroupIDs,
 		Attributes: infrav1.ClassicELBAttributes{
 			IdleTimeout: 10 * time.Minute,
 		},

dropbox-code/repo-9

api/v1alpha3/awscluster_types.go
@@ -131,6 +131,11 @@ type AWSLoadBalancerSpec struct {
 	// Subnets sets the subnets that should be applied to the control plane load balancer (defaults to discovered subnets for managed VPCs or an empty set for unmanaged VPCs)
 	// +optional
 	Subnets []string `json:"subnets,omitempty"`
+
+	// AdditonalSecurityGroups sets the security groups used by the load balancer. Expected to be security group IDs.
+	// This is optional - if not provided new security groups will be created for the load balancer
+	// +optional
+	AdditionalSecurityGroups []string `json:"securityGroups,omitempty"`
 }
 
 // AWSClusterStatus defines the observed state of AWSCluster

pkg/cloud/services/elb/loadbalancer.go
@@ -304,6 +304,14 @@ func (s *Service) getAPIServerClassicELBSpec() (*infrav1.ClassicELB, error) {
 	if err != nil {
 		return nil, err
 	}
+
+	securityGroupIDs := []string{}
+	controlPlaneLoadBalancer := s.scope.ControlPlaneLoadBalancer()
+	if controlPlaneLoadBalancer != nil && len(controlPlaneLoadBalancer.AdditionalSecurityGroups) != 0 {
+		securityGroupIDs = append(securityGroupIDs, controlPlaneLoadBalancer.AdditionalSecurityGroups...)
+	}
+	securityGroupIDs = append(securityGroupIDs, s.scope.SecurityGroups()[infrav1.SecurityGroupAPIServerLB].ID)
+
 	res := &infrav1.ClassicELB{
 		Name:   elbName,
 		Scheme: s.scope.ControlPlaneLoadBalancerScheme(),
@@ -322,7 +330,7 @@ func (s *Service) getAPIServerClassicELBSpec() (*infrav1.ClassicELB, error) {
 			HealthyThreshold:   5,
 			UnhealthyThreshold: 3,
 		},
-		SecurityGroupIDs: []string{s.scope.SecurityGroups()[infrav1.SecurityGroupAPIServerLB].ID},
+		SecurityGroupIDs: securityGroupIDs,
 		Attributes: infrav1.ClassicELBAttributes{
 			IdleTimeout: 10 * time.Minute,
 		},