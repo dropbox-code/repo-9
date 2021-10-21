Madhur97/cluster-api-provider-aws

Adding optional IAM policy creation for EKS Console

cmd/clusterawsadm/cloudformation/bootstrap/user.go
@@ -45,3 +45,25 @@ func (t Template) bootstrapUserPolicy() []cfn_iam.User_Policy {
 	}
 	return userPolicies
 }
+
+func (t Template) eksConsolePolicies() *v1alpha4.PolicyDocument {
+	return &v1alpha4.PolicyDocument{
+		Version: v1alpha4.CurrentVersion,
+		Statement: []v1alpha4.StatementEntry{
+			{
+				Effect:   v1alpha4.EffectAllow,
+				Resource: v1alpha4.Resources{v1alpha4.Any},
+				Action: v1alpha4.Actions{
+					"eks:DescribeNodegroup",
+					"eks:ListNodegroups",
+					"eks:DescribeCluster",
+					"eks:ListClusters",
+					"eks:AccessKubernetesApi",
+					"ssm:GetParameter",
+					"eks:ListUpdates",
+					"eks:ListFargateProfiles",
+				},
+			},
+		},
+	}
+}

dropbox-code/repo-9

cmd/clusterawsadm/cloudformation/bootstrap/user.go
@@ -45,3 +45,25 @@ func (t Template) bootstrapUserPolicy() []cfn_iam.User_Policy {
 	}
 	return userPolicies
 }
+
+func (t Template) eksConsolePolicies() *iamv1.PolicyDocument {
+	return &iamv1.PolicyDocument{
+		Version: iamv1.CurrentVersion,
+		Statement: []iamv1.StatementEntry{
+			{
+				Effect:   iamv1.EffectAllow,
+				Resource: iamv1.Resources{iamv1.Any},
+				Action: iamv1.Actions{
+					"eks:DescribeNodegroup",
+					"eks:ListNodegroups",
+					"eks:DescribeCluster",
+					"eks:ListClusters",
+					"eks:AccessKubernetesApi",
+					"ssm:GetParameter",
+					"eks:ListUpdates",
+					"eks:ListFargateProfiles",
+				},
+			},
+		},
+	}
+}